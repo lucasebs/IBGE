cffbd2b0b678b057d51bb3b30761006ae3261bce
lucasebs <lucasebatista@gmail.com>
2018-04-18 23:29:45 -0300

    Add fields 'pesquisa_id' and 'pesquisa_nome' at indicadores.csv

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def open_file():
 	file = open('CSV/indicadores.csv', 'a')
-	file.write('id;nome;posicao\n')
+	file.write('pesquisa_id;pesquisa_nome;id;nome;posicao\n')
 	return file
 
 def close_file(file):
@@ -18,18 +18,18 @@ def get_url(pesquisa, source):
 
 def get_pesquisas():
 	df = pd.read_csv('CSV/pesquisas.csv', error_bad_lines=False, sep=';')
-	df = df['id']	
+	# df = df['id']	
 	return df
 
-def write_indicadores(id, posicao, indicador, file):
-	file.write(id + ';' + posicao + ';' + indicador + '\n')
+def write_indicadores(pesquisa, pesquisa_nome, id, posicao, indicador, file):
+	file.write(pesquisa + ';' + pesquisa_nome + ';' + id + ';' + posicao + ';' + indicador + '\n')
 
 def crawl(source,file):
 	cont = 0
 	pesquisas = get_pesquisas()
 	# pesquisas = 
 	print(pesquisas)
-	for pesquisa in pesquisas:
+	for pesquisa in pesquisas['id']:
 		if pesquisa is not 'id':
 			print(pesquisa)
 			response = rq.get(get_url(pesquisa, source))
@@ -38,22 +38,21 @@ def crawl(source,file):
 			cont += 1
 			# print(cont)
 
-		set_indicadores(indicadores, file)
+		set_indicadores(pesquisa, pesquisas['nome'][cont], indicadores, file)
 
-def set_indicadores(indicadores, file):
+def set_indicadores(pesquisa, pesquisa_nome, indicadores, file):
 	for indicador in indicadores:	
 		# print(indicador['id'],indicador['posicao'],indicador['indicador'])
-		write_indicadores(str(indicador['id']),str(indicador['posicao']),indicador['indicador'], file)		
+		write_indicadores(str(pesquisa), pesquisa_nome, str(indicador['id']),str(indicador['posicao']),indicador['indicador'], file)		
 		if indicador['children'] is not None:
-			set_indicadores(indicador['children'], file)
+			set_indicadores(pesquisa, pesquisa_nome, indicador['children'], file)
 
 def main():
 	file = open_file()
 	source = 'https://servicodados.ibge.gov.br/api/v1/pesquisas/'
-	crawl(source,file)
-	close_file(file)    
+	crawl(source,file)	
 
-	# print(get_pesquisas())
+	close_file(file)    
 
 if __name__ == "__main__":
     main()
